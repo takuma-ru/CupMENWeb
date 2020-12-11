cb7fcafff5128e2bbf9d3e38be404667db5bb0f4
takuma-ru <kcattdev@gmail.com>
2020-12-11 16:27:25 +0900

    Fix layouts/default line20

--- store/index.js
+++ store/index.js
@@ -6,14 +6,11 @@ import [変数名] from '相対パス'
 state = () => ({
   importした変数名
 })
-
-・
-vueファイルで変数を呼び出す際は、 $store.state.[変数名]
+・vueファイルで変数を呼び出す際は、 $store.state.[変数名]
 */
 
 import sanyoo from '../assets/json/sanyoo_item.json'
 
 export const state = () => ({
-  message: 'Hello World',
   sanyoo
 })
