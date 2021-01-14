5cff0f095ca09cff865d9d5c2cc808dc18da1b0a
takuma-ru <kcattdev@gmail.com>
2021-01-14 23:29:53 +0900

    Add cupnoodle.jpg, Update index.vue & Top_CompanyLink.vue

--- components/Top_CompanyLink.vue
+++ components/Top_CompanyLink.vue
@@ -1,5 +1,11 @@
 <template>
   <div @click="Show()" class="block item">
+    <div class="back_img">
+      <img 
+        class="front_img"
+        v-bind:src="src"
+      />
+    </div>
     <div class="title">{{title}}</div>
   </div>
 </template>
@@ -8,12 +14,13 @@
 export default {
   data(){
     return{
-
+      src: require("../assets/img/" + this.img_src)
     }
   },
 
   props: [
     'title',
+    'img_src',
     'url'
   ],
 
@@ -28,20 +35,50 @@ export default {
 
 <style>
 .block{
+  position: relative;
   width: 100%;
   height: 20vh;
   margin-bottom: 1vh;
+  margin-left: 5px;
+  margin-right: 5px;
+  color: white;
+  overflow: hidden;
   cursor: pointer;
   background-color: grey;
-  transition-duration: 0.3s;
+  transition-duration: 0.5s;
+}
+
+.back_img {
+  width: 100%;
+  height: 20vh;
+  background: #000;
 }
 
 .block:hover{
-  color: white;
-  transition-duration: 0.3s;
+  color: black;
+  transition-duration: 0.5s;
+}
+
+.front_img {
+  width: 100%;
+  height: 100%;
+  object-fit: cover;
+  filter: opacity(0.3) grayscale(100%);
+  transition-duration: 0.5s;
+}
+
+.block:hover .front_img {
+  transform: scale(1.05);
+  filter: opacity(1) grayscale(0%);
+  transition-duration: 0.25s;
 }
 
 .title{
+  position: absolute;
+  top: 0%;
+  left: 50%;
+  transform: translate(-50%, 0%);
+  box-sizing: border-box;
   font-size: 3.5em;
   line-height: 20vh;
   text-align: center;
--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,26 @@
 <template>
-  <div>
-    <div v-for="(item, i) in $store.state.toplink" :key="i">
-      <comlink :title="item.title" :url="item.url"></comlink>
+  <div class="index_container">
+    <div class="item top">
+      <div class="top_text">カップ麺は高専生のいのち</div>
+    </div>
+
+    <comlink
+      class="link_history"
+      :title="'カップ麺の歴史'"
+      :img_src="'cupnoodle.jpg'"
+      :url="'/Nissin'"
+    />
+
+    <div
+      v-for="(item, i) in $store.state.toplink"
+      :key="i"
+    >
+      <comlink
+        class="link"
+        :title="item.title"
+        :img_src="item.img"
+        :url="item.url"
+      />
     </div>
   </div>
 </template>
@@ -25,3 +44,44 @@ export default {
   }
 }
 </script>
+
+<style>
+.index_container{
+  display: flex;
+  flex-direction: row;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+.top{
+  position: relative;
+  width: 99%;
+  height: 40vh;
+  margin-bottom: 2vh;
+  background-color: rgb(175, 175, 175);
+}
+
+.top_text{
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+
+.link{
+  width: 48vw;
+}
+
+.link_history{
+  width: 70vw;
+}
+
+@media screen and (max-width: 960px) { 
+  .link{
+    width: 100%;
+  }
+  .link_history{
+    width: 100%;
+  }
+}
+</style>
