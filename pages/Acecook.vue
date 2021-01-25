a1c872d9dfaf67ebf46c5c36f6fe4b8436250b76
SuzuSys <sigmatics@outlook.jp>
2021-01-26 08:26:01 +0900

    update

--- pages/Acecook.vue
+++ pages/Acecook.vue
@@ -1,3 +1,65 @@
 <template>
-<p>エースコック</p>
+  <div>
+    <div class="top_title top_font">エースコック</div>
+    <Top :about="about" :slogan="slogan" :brand="brand"></Top>
+    <div class="items">
+    <div class="tag">
+      <div class="tag_text">
+        <span style="margin-left: 1vw; margin-right: 1vw;">商品紹介</span>
+      </div>
+    </div>
+    <div class="wrap">
+      <Article class="item" v-for="(item, i) in $store.state.acecook" :key="i" :articleItem="item"></Article>
+    </div>
+    </div>
+    <div class="source">
+      出典:
+      {{source}}
+    </div>
+  </div>
 </template>
+
+<script>
+import Top from '../components/AboutCompany'
+import Article from '../components/ArticleNormal'
+
+export default {
+  data(){
+    return{
+      about: "<ul><li>名称: エースコック株式会社</li><li>創業: 1954年1月</li><li>資本金: 19億円</li><li>従業員数: 738人(2017/3現在)</li></ul>",
+      slogan: "Cook happiness おいしい しあわせ つくりたい",
+      brand: [],
+      source: ""
+    }
+  },
+  components: {
+    Top,
+    Article
+  }
+}
+</script>
+
+<style>
+.items{
+  margin-top: 24px;
+}
+.top_title{
+  text-align: center;
+}
+.wrap{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+@media (min-width: 300px) {
+  .top_font {
+    font-size: calc(2.25rem + ((1vw - 3px) * 3.0864));
+  }
+}
+@media (min-width: 1920px) {
+  .top_font {
+    font-size: 86px;
+  }
+}
+</style>
--- pages/Overseas.vue
+++ pages/Overseas.vue
@@ -1,3 +1,61 @@
 <template>
-<p>海外のカップ麺</p>
+  <div>
+    <div class="top_title top_font">海外のカップ麺</div>
+    <div class="items">
+    <div class="tag">
+      <div class="tag_text">
+        <span style="margin-left: 1vw; margin-right: 1vw;">商品紹介</span>
+      </div>
+    </div>
+    <div class="wrap">
+      <Article class="item" v-for="(item, i) in $store.state.overseas" :key="i" :articleItem="item"></Article>
+    </div>
+    </div>
+    <div class="source">
+      出典:
+      {{source}}
+    </div>
+  </div>
 </template>
+
+<script>
+import Top from '../components/AboutCompany'
+import Article from '../components/ArticleOverseas'
+
+export default {
+  data(){
+    return{
+      
+    }
+  },
+  components: {
+    Top,
+    Article
+  }
+}
+</script>
+
+<style>
+.items{
+  margin-top: 24px;
+}
+.top_title{
+  text-align: center;
+}
+.wrap{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+
+@media (min-width: 300px) {
+  .top_font {
+    font-size: calc(2.25rem + ((1vw - 3px) * 3.0864));
+  }
+}
+@media (min-width: 1920px) {
+  .top_font {
+    font-size: 86px;
+  }
+}
+</style>
